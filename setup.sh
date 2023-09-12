d305e08dda6f0e20fde6573b90905b9757709bc1
tharindu899 <tprbath81@gmail.com>
2023-09-12 21:40:10 +0530

    all

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 setup () {
 	printf "\n\033[1;92m Installing Requirements..\n\n"
-	apt update
-	apt upgrade
-	apt install python
-	apt install figlet
-	apt install git
-	apt install wget
-	apt install zsh
-	apt install curl
+	apt update -y
+	apt upgrade -y
+	apt install python -y
+	apt install figlet -y
+	apt install git -y
+	apt install wget -y
+	apt install zsh -y
+	apt install curl -y
+	apt install lsd -y
+	apt install micro -y
 	printf "\n\033[1;92m All installed\n"
 	sleep 3
 	clear
@@ -15,6 +17,10 @@ setup () {
 	echo "#!/data/data/com.termux/files/usr/bin/sh" >> zsh2
 	echo "bash ~/zsh-theme/zsh.sh" >> zsh2
 	chmod 777 zsh2
-	printf "\n\n\033[1;92m Now you can use :- zsh2\n"
+	echo "#!/data/data/com.termux/files/usr/bin/sh" >> add
+	echo "bash ~/add.sh" >> add
+	chmod 777 add
+	printf "\n\n\033[1;92m Now you can use :- zsh2"
+	printf "\n\n\033[1;92m use to create alias :- add\n"
 	}
 	setup
