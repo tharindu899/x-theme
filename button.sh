3139feb7695d91fc2eff1cb421779bb6b1da2ebd
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-09-13 21:38:59 +0530

    update button.sh

--- button.sh
+++ button.sh
@@ -1,49 +1,66 @@
+#!/bin/bash
+
+# Define ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+RESET='\033[0m' # No Color
+
 R () {
-	cd $PREFIX/bin
-	if [ -e python ];then
-	python ~/zsh-theme/.random.py
-	else
-	pkg install python
-	python ~/zsh-theme/.random.py
-	fi
-	}
-	fl () {
-		cd ~/zsh-theme
-		}
-	ba() {
-		R
-		figlet ZSH-THEME
-		echo
-		}
-		e() {
-			cd $PREFIX/bin
-			echo "#!/data/data/com.termux/files/usr/bin/sh" >> e
-			echo "killall -9 com.termux" >> e
-			chmod 777 e
-			}
-		menu3 () {
-			ba
-			cd ~
-			if [ -e .termux ];then
-			R
-			cd ~/.termux
-			pwd
-			rm -f termux.properties > /dev/null 2>&1
-			printf "\n\n\033[1;92m Downloading button file \033[0m\n\n"
-			wget https://raw.githubusercontent.com/tharindu899/addon/main/termux/zsh/termux.properties
-			e
-			R
-			printf "\n\033[1;92m Restart termux and check \033[0m\n\n"
-			else
-			R
-			mkdir ~/.termux
-			cd ~/.termux
-			rm -f ~/.termux/termux.properties > /dev/null 2>&1
-			printf "\n\033[1;92m Downloading button \033[0m\n\n"
-			wget https://raw.githubusercontent.com/tharindu899/addon/main/termux/zsh/termux.properties
-			e
-			R
-			printf "\nRestart termux and check\n"
-			fi
-			}
-			menu3
+  cd $PREFIX/bin
+  if [ -e python ]; then
+    python ~/zsh-theme/.random.py
+  else
+    pkg install python
+    python ~/zsh-theme/.random.py
+  fi
+}
+
+fl () {
+  cd ~/zsh-theme
+}
+
+ba() {
+  R
+  printf "${GREEN}ZSH-THEME${RESET}\n"
+  echo
+}
+
+ex() {
+  cd $PREFIX/bin
+  echo "#!/data/data/com.termux/files/usr/bin/sh" >> ex
+  echo "killall -9 com.termux" >> ex
+  chmod 777 ex
+}
+
+menu3 () {
+  ba
+  cd ~
+  if [ -e .termux ]; then
+    R
+    cd ~/.termux
+    pwd
+    rm -f termux.properties > /dev/null 2>&1
+    printf "\n\n${YELLOW}Downloading button file${RESET}\n\n"
+    wget https://raw.githubusercontent.com/tharindu899/addon/main/termux/zsh/termux.properties
+    ex
+    R
+    printf "\n${GREEN}Restart Termux and check${RESET}\n"
+  else
+    R
+    mkdir ~/.termux
+    cd ~/.termux
+    rm -f ~/.termux/termux.properties > /dev/null 2>&1
+    printf "\n\n${YELLOW}Downloading button file${RESET}\n\n"
+    wget https://raw.githubusercontent.com/tharindu899/addon/main/termux/zsh/termux.properties
+    ex
+    R
+    printf "\n${GREEN}Restart Termux and check${RESET}\n"
+  fi
+}
+
+menu3
