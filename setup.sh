319e8a58343b71d2ce0dd497de969adcee0485e5
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-09-13 21:41:49 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,49 +1,63 @@
+#!/bin/bash
+
+# Define ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+RESET='\033[0m' # No Color
+
 setup () {
-	printf "\n\033[1;92m Installing Requirements..\033[0m\n\n"
-	printf "\n\033[1;92m apt update \033[0m\n\n"
-	apt update
-	printf "\n\033[1;92m apt upgrade \033[0m\n\n"
-	apt upgrade -y
-	clear
-	printf "\n\033[1;92m python \033[0m\n\n"
-	apt install python -y
-	clear
-	printf "\n\033[1;92m figlet \033[0m\n\n"
-	apt install figlet -y
-	clear
-	printf "\n\033[1;92m git \033[0m\n\n"
-	apt install git -y
-	clear
-	printf "\n\033[1;92m wget \033[0m\n\n"
-	apt install wget -y
-	clear
-	printf "\n\033[1;92m zsh \033[0m\n\n"
-	apt install zsh -y
-	clear
-	printf "\n\033[1;92m curl \033[0m\n\n"
-	apt install curl -y
-	clear
-	printf "\n\033[1;92m lsd \033[0m\n\n"
-	apt install lsd -y
-	clear
-	printf "\n\033[1;92m micro \033[0m\n\n"
-	apt install micro -y
-	printf "\n\033[1;92m zip/unzip \033[0m\n\n"
-	pkg install zip -y
-	#cp -r add.sh /data/data/com.termux/files
-	printf "\n\033[1;92m AcodeX \033[0m\n\n"
-	curl -sL https://raw.githubusercontent.com/bajrangCoder/acode-plugin-acodex/main/installServer.sh | bash
-	printf "\n\033[1;92m All installed \033[0m\n\n"
-	sleep 3
-	clear
-	cd $PREFIX/bin
-	echo "#!/data/data/com.termux/files/usr/bin/sh" >> zsh2
-	echo "bash ~/zsh-theme/zsh.sh" >> zsh2
-	chmod 777 zsh2
-	echo "#!/data/data/com.termux/files/usr/bin/sh" >> add
-	echo "bash ~/add.sh" >> add
-	chmod 777 add
-	printf "\n\n\033[1;92m Now you can use\033[0m :- zsh2"
-	printf "\033[1;92m Now you can use to create alias\033[0m:- add\n"
-	}
-	setup
+  printf "\n${GREEN}Installing Requirements..${RESET}\n\n"
+  printf "\n${YELLOW}apt update${RESET}\n\n"
+  apt update
+  printf "\n${YELLOW}apt upgrade${RESET}\n\n"
+  apt upgrade -y
+  clear
+  printf "\n${GREEN}python${RESET}\n\n"
+  apt install python -y
+  clear
+  printf "\n${BLUE}figlet${RESET}\n\n"
+  apt install figlet -y
+  clear
+  printf "\n${MAGENTA}git${RESET}\n\n"
+  apt install git -y
+  clear
+  printf "\n${CYAN}wget${RESET}\n\n"
+  apt install wget -y
+  clear
+  printf "\n${WHITE}zsh${RESET}\n\n"
+  apt install zsh -y
+  clear
+  printf "\n${GREEN}curl${RESET}\n\n"
+  apt install curl -y
+  clear
+  printf "\n${YELLOW}lsd${RESET}\n\n"
+  apt install lsd -y
+  clear
+  printf "\n${BLUE}micro${RESET}\n\n"
+  apt install micro -y
+  clear
+  printf "\n${YELLOW}zip/unzip${RESET}\n\n"
+  pkg install zip -y
+  clear
+  printf "\n${MAGENTA}AcodeX${RESET}\n\n"
+  curl -sL https://raw.githubusercontent.com/bajrangCoder/acode-plugin-acodex/main/installServer.sh | bash
+  printf "\n${GREEN}All installed${RESET}\n\n"
+  sleep 3
+  clear
+  cd $PREFIX/bin
+  echo "#!/data/data/com.termux/files/usr/bin/sh" >> zsh2
+  echo "bash ~/zsh-theme/zsh.sh" >> zsh2
+  chmod 777 zsh2
+  echo "#!/data/data/com.termux/files/usr/bin/sh" >> add
+  echo "bash ~/add.sh" >> add
+  chmod 777 add
+  printf "\n\n${CYAN}Now you can use:- zsh2${RESET}\n"
+  printf "\n${YELLOW}Now you can use to create alias:- add${RESET}\n"
+}
+
+setup
