78156ab50e5f3f50b07555bd36dc55dd8430c0ce
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-09-14 01:05:03 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,42 +11,42 @@ WHITE='\033[1;37m'
 RESET='\033[0m' # No Color
 
 setup () {
-  printf "\n\n ☢ ${GREEN}Installing Requirements..${RESET}\n\n"
-  printf "\n\n ☢ ${YELLOW}apt update${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${GREEN}Installing Requirements..${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${YELLOW}apt update${RESET}\n\n"
   apt update
-  printf "\n\n ☢ ${YELLOW}apt upgrade${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${YELLOW}apt upgrade${RESET}\n\n"
   apt upgrade -y
   clear
-  printf "\n\n ☢ ${GREEN}python${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${GREEN}python${RESET}\n\n"
   apt install python -y
   clear
-  printf "\n\n ☢ ${BLUE}figlet${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${BLUE}figlet${RESET}\n\n"
   apt install figlet -y
   clear
-  printf "\n\n ☢ ${MAGENTA}git${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${MAGENTA}git${RESET}\n\n"
   apt install git -y
   clear
-  printf "\n\n ☢ ${CYAN}wget${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${CYAN}wget${RESET}\n\n"
   apt install wget -y
   clear
-  printf "\n\n ☢ ${WHITE}zsh${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${WHITE}zsh${RESET}\n\n"
   apt install zsh -y
   clear
-  printf "\n\n ☢ ${GREEN}curl${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${GREEN}curl${RESET}\n\n"
   apt install curl -y
   clear
-  printf "\n\n ☢ ${YELLOW}lsd${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${YELLOW}lsd${RESET}\n\n"
   apt install lsd -y
   clear
-  printf "\n\n ☢ ${BLUE}micro${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${BLUE}micro${RESET}\n\n"
   apt install micro -y
   clear
-  printf "\n\n ☢ ${YELLOW}zip/unzip${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${YELLOW}zip/unzip${RESET}\n\n"
   pkg install zip -y
   clear
-  printf "\n\n ☢ ${MAGENTA}AcodeX${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${MAGENTA}AcodeX${RESET}\n\n"
   curl -sL https://raw.githubusercontent.com/bajrangCoder/acode-plugin-acodex/main/installServer.sh | bash
-  printf "\n\n ☢ ${GREEN}All installed${RESET}\n\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${GREEN}All installed${RESET}\n\n"
   sleep 3
   clear
   cd $PREFIX/bin
@@ -58,8 +58,8 @@ setup () {
   echo "#!/data/data/com.termux/files/usr/bin/sh" >> add
   echo "bash ~/add.sh" >> add
   chmod 777 add
-  printf "\n\n ☢ ${CYAN}Now you can use:- zsh2${RESET}\n"
-  printf "\n\n ☢ ${YELLOW}Now you can use to create alias:- add${RESET}\n"
+  printf "\n\n${RED}  ☢ ${RESET} ${CYAN}Now you can use${RESET}:- zsh2\n"
+  printf "${RED}  ☢ ${RESET} ${YELLOW}Now you can use to create alias${RESET}:- add\n"
 }
 
 setup
