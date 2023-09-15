7ce0b0551baf102d9d02c11f5a9b8b5d43b722f1
tharindu899 <tprbath81@gmail.com>
2023-09-15 23:07:14 +0530

    colour

--- start-zsh.sh
+++ start-zsh.sh
@@ -4,13 +4,6 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
-ORANGE='\033[0;91m'
-PINK='\033[0;95m'
-GRAY='\033[0;90m'
 NC='\033[0m' # No Color
 
 R () {
