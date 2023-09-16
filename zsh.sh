b44be0ccb99531298766aaf2446a69647ddc8a72
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-09-16 12:52:02 +0530

    update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -49,7 +49,7 @@ menu() {
     printf "\033[1;92m[\033[0m2\033[1;92m]\033[1;93m Termux Extra button\n"
     printf "\033[1;92m[\033[0m3\033[1;92m]\033[1;93m Create banner save com:(ctrl+x y)\n"
     printf "\033[1;92m[\033[0m4\033[1;92m]\033[1;93m Exit\n\n\n"
-    printf "\033[0;32m  Termux bannar save to use this command (ctrl+v y)\033[0m \n\033[0;31m  do you want defult bannar don't use (no-03) button \033[0m\n\n"
+    printf "\033[0;32m  Termux bannar save to use this command (ctrl+v y)\033[0m \n\033[0;31m  do you want defult bannar then exit (don't use {no-03} button) \033[0m\n\n"
     printf "\033[1;96m%swellcom >> "
     read a
     case $a in
