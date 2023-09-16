dec63bbe78eafda3b1895ee23b73496e840311f8
Tharindu Prabath <116081100+tharindu899@users.noreply.github.com>
2023-09-16 14:47:03 +0530

    update button.sh

--- button.sh
+++ button.sh
@@ -49,7 +49,7 @@ menu3 () {
     wget https://raw.githubusercontent.com/tharindu899/addon/main/termux/zsh/termux.properties
     ex
     R
-    printf "\n\n    💠 ${GREEN}Restart Termux and check${RESET}\n"
+    printf "\n\n    💠 ${GREEN}Restart Termux and check${RESET}"
     printf "\n    💠 ${YELLOW}Create bannar & button${NC}:- ${GREEN}zsh2${NC}\n\n"
   else
     R
